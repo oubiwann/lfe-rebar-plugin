881477231a938f6b295028eeeb2c5c6f3c2687cf
Duncan McGreggor <duncan@cogitat.io>
2014-03-11 23:58:50 -0500

    Added EXPM file.

---
+++ package.exs
@@ -0,0 +1,7 @@
+Expm.Package.new(
+  name: "lfe-rebar-plugin",
+  description: "lfe-rebar-plugin DESCRIPTION",
+  version: "0.0.1",
+  keywords: ["LFE", "Lisp", "Library", "API"],
+  maintainers: [[name: "YOUR NAME", email: "YOUR@EMAIL.com"]],
+  repositories: [[github: "YOUR_GITHUB_NAME/lfe-rebar-plugin]])
